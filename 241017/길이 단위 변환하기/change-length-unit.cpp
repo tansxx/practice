cf28fa21822d2d4b38604049d9266f35cf8e94a6
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-17 01:51:25 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241017/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double ft = 30.48, mi = 160934;
+    cout << fixed;
+    cout.precision(1);
+
+    cout << "9.2ft" << " = " << 9.2 * ft << "cm" << endl;
+    cout << "1.3mi" << " = " << 1.3 * mi << "cm";
+    return 0;
+}

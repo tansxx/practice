76dd41c468a1fe15e3a93750ec2dc77521372afc
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-18 01:27:44 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char i;
+    int m, d, y;
+
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> y;
+    cout << y << "." << m << "." << d; 
+    return 0;
+}

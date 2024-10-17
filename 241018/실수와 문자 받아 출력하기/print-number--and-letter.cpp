11bbf8a3b320a12981a49d86c2b51c2fa1080970
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-18 01:06:15 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    double a, b;
+    cin >> c >> b >> a;
+    cout << fixed;
+    cout.precision(2);
+    cout << c << endl << b << endl << a; 
+    return 0;
+}

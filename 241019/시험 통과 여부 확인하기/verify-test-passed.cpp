fea90c49ec2e7d241b5e7f5706175c07bf66de8f
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-19 02:24:02 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241019/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    if (n >= 80) {
+        cout << ("pass") << endl;
+    } else {
+        cout << 100 - n << " more score";
+    }
+    return 0;
+}

e13c8fd8da5326b0766cc5e2117df0455721e097
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-19 00:29:23 +0900

    Title: 합과 차의 나눗셈, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241019/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let add = a + b;
+let minus = a - b;
+
+console.log((add / minus).toFixed(2));

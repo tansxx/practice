ded06f8e25e3a6bfc188559bfe43f8108c967f76
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-19 00:07:24 +0900

    Title: 두수의 곱과 몫, Time: 161ms, Memory: 14MB, Status: Passed - CodeTree

--- 241019/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 241019/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -4,5 +4,5 @@ let input = fs.readFileSync(0).toString().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-console.log(`${a} * ${b} = ` + a * b);
-console.log(`${a} / ${b} = ` + a / b);
+console.log(`${a} * ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${parseInt(a / b)}`);

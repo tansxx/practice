a816afc524a69d0d89e30fa510a902107e11a4c8
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-19 23:54:30 +0900

    Title: 삼항연산자, Time: 162ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 241019/삼항연산자/ternary-operator.js
+++ 241019/삼항연산자/ternary-operator.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let score = fs.readFileSync(0).toString().trim();
 
-let grade = score = 100 ? "pass" : "failure";
-
-console.log(grade);
+let grade = score = 100 ? 0 : 1;
+let result = grade = 0 ? "pass" : "failure";
+console.log(result);

41347d7ec25dd9633cd0489ae6fb745a0fb09e7f
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-23 03:35:56 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/윤년인가/is-leap-year.cpp
+++ 241023/윤년인가/is-leap-year.cpp
@@ -5,9 +5,9 @@ int main() {
     int y;
     cin >> y;
 
-    if (y % 4 == 0) {
+    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
         cout << "true";
-    } else if (y % 100 == 0 && y % 400 == 0) {
+    } else {
         cout << "false";
     }
     return 0;

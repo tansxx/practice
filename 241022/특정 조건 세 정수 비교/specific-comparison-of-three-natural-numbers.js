1b16c8c05f7c2456fc3ab05d4b351e3c00df8c60
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-22 14:17:00 +0900

    Title: 특정 조건 세 정수 비교, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241022/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let res1, res2;
+
+if (a <= b && a <= c) {
+    res1 = 1;
+} else {
+    res1 = 0;
+}
+
+if (a === b === c) {
+    res2 = 1;
+} else {
+    res2 = 0;
+}
+
+console.log(res1, res2);

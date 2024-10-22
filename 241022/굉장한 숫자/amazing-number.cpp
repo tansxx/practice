bd42fc586864833870bd9059d9274b1cd903d3e4
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-22 15:20:56 +0900

    Title: 굉장한 숫자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/굉장한 숫자/amazing-number.cpp
+++ 241022/굉장한 숫자/amazing-number.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     cin >> n;
 
-    if ((n % 2 != 0 && n % 3 == 0) || (n % 2 == 0 || n % 5 == 0)) {
+    if ((n % 2 != 0 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)) {
         cout << "true";
     } else {
         cout << "false";

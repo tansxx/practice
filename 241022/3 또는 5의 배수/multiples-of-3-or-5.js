723bd732c5840d45c6b36207207249c8e1c3834d
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-22 12:32:47 +0900

    Title: 3 또는 5의 배수, Time: 236ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241022/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if (a % 3 === 0) {
+    console.log("YES");
+} else {
+    console.log("NO");
+}
+
+
+if (a % 5 === 0) {
+    console.log("YES");
+} else {
+    console.log("NO");
+}

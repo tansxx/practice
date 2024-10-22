b856a22acdbab66fdc03a041eeafc17e44bba274
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-22 13:17:56 +0900

    Title: 4가지 관계연산자, Time: 170ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241022/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+console.log(a >= b ? 1 : 0);
+console.log(a > b ? 1 : 0);
+console.log(b >= a ? 1 : 0);
+console.log(b > a ? 1 : 0);

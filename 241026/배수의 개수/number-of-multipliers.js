348b762137634dff93a3fb3050026a30b816c75d
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-26 17:18:24 +0900

    Title: 배수의 개수, Time: 236ms, Memory: 14MB, Status: Passed - CodeTree

--- 241026/배수의 개수/number-of-multipliers.js
+++ 241026/배수의 개수/number-of-multipliers.js
@@ -1,17 +1,18 @@
+// 변수 선언 및 입력
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(" ");
+let input = fs.readFileSync(0).toString().trim().split("\n");
 let cntA = 0, cntB = 0;
 
 for (let i = 0; i < 10; i++) {
     let x = Number(input[i]);
-
     if (x % 3 === 0) {
         cntA++;
-    } 
-    
+    }
+
     if (x % 5 === 0) {
         cntB++;
     }
 }
 
+// 출력
 console.log(cntA, cntB);

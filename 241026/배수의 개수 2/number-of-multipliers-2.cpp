f60334429d8150b0b8a417801737ec96d5b8b8ce
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-26 17:10:23 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241026/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, cnt = 0;
+    
+    for (int i = 1; i <= 10; i++) {
+        cin >> a;
+
+        if (a % 2 == 1) {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}

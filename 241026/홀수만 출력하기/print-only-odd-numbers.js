18681431a0dfc01ea3f961d4e8a481c301d7c07b
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-26 16:42:46 +0900

    Title: 홀수만 출력하기, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 241026/홀수만 출력하기/print-only-odd-numbers.js
+++ 241026/홀수만 출력하기/print-only-odd-numbers.js
@@ -1,15 +1,11 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
+let n = Number(input[0]);
 
-// 첫 번째 줄에서 N을 가져옴
-let N = Number(input[0]);
+for (let i = 1; i <= n; i++) {
+    let x = Number(input[i]);
 
-// 두 번째 줄부터 N개의 정수를 배열로 변환
-let numbers = input.slice(1, N + 1).map(Number);
-
-for (let i = 0; i < N; i++) {
-    // 조건: 홀수이면서 3의 배수인지 확인
-    if (numbers[i] % 2 !== 0 && numbers[i] % 3 === 0) {
-        console.log(numbers[i]);
+    if (x % 2 === 1 && x % 3 === 0) {
+        console.log(x);
     }
 }

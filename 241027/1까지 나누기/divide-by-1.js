aa70cd3320fd82a572adddc71f1b6c661dd9d670
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-27 17:19:19 +0900

    Title: 1까지 나누기, Time: 191ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241027/1까지 나누기/divide-by-1.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let cnt = 0;
+
+for (let i = 1; ; i++) {
+    n = parseInt(n / i);
+    cnt++;
+
+    if (n <= 1) {
+        break;
+    }
+}
+
+console.log(cnt);

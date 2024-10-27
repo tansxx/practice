4919c2525e69b228f09810e6a15c0c28df064744
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-28 02:42:09 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241028/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let index = 0;
+
+while (true) {
+
+    let arr = input[index].split(" ");
+    let x = Number(arr[0]);
+    let y = Number(arr[1]);
+    let c = arr[2];
+
+    index++;
+
+    console.log(x * y);
+    if (c === "C") {
+        break;
+    }  
+}

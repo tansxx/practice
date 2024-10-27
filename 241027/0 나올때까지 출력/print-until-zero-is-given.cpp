269882d7455c5aa47e86b782ba282c9a568c86f6
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-27 17:57:10 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241027/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while (true) {
+        int n;
+        cin >> n;
+        if (n == 0) {
+            break;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}

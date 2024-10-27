88156603f21ecb4e5e3189b3f2cd9b1f0866e4d0
tansxx <159680008+tansxx@users.noreply.github.com>
2024-10-27 18:10:58 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241027/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    while (true) {
+        int n;
+        cin >> n;
+
+        if (n == 1) {
+            cout << "John" << endl;
+        } else if (n == 2) {
+            cout << "Tom" << endl;
+        } else if (n == 3) {
+            cout << "Paul" << endl;
+        } else if (n == 4) {
+            cout << "Sam" << endl;
+        } else {
+            cout << "Vacancy";
+            break;
+        }
+    }
+    return 0;
+}
